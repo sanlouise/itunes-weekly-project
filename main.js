a4b1bc18f1f85e959409ea07ab7a1112fa1862be
sanlouise <sandra.adamshallie@gmail.com>
2017-07-19 14:50:27 -0400

    Add basic js functionality

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
+const player = document.querySelector(".player");
+const musicPlayer = document.querySelector(".music-player");
+const search = document.querySelector(".search");
+const searchForm = document.querySelector(".search-form");
+const results = document.querySelector(".results");
+const searchField = document.querySelector(".search-field");
+const searchButton = document.querySelector(".search-button");
+const songsUl = document.querySelector("ul");
+const searchTerm = "beatles";
 
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
+function fetchData(){
+  let url = `https://itunes.apple.com/search?term=${searchTerm}`
+
+  fetch(url)
+  .then((response) => response.json())
+  .then(data => {
+
+    for (let i = 0; i < data.results.length; i++) {
+      const songLi = document.createElement("li");
+      const artworkUrl = document.createElement("img");
+      const trackName = document.createElement("p");
+      const artistName = document.createElement("p");
+
+      artworkUrl.src = data.results[i].artworkUrl100;
+      trackName.textContent = data.results[i].trackName;
+      artistName.textContent = data.results[i].artistName;
+
+      songLi.appendChild(artworkUrl);
+      songLi.appendChild(trackName);
+      songLi.appendChild(artistName);
+      songsUl.appendChild(songLi);
+    }
+  })
+}
+
+fetchData();
