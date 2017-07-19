6a9501950001da926c4c6d5de2bf36c9f22c08ae
sanlouise <sandra.adamshallie@gmail.com>
2017-07-19 16:22:05 -0400

    Finalize basic music player functionality

--- main.js
+++ main.js
@@ -5,34 +5,39 @@ const searchForm = document.querySelector(".search-form");
 const results = document.querySelector(".results");
 const searchField = document.querySelector(".search-field");
 const searchButton = document.querySelector(".search-button");
-const songsUl = document.querySelector("ul");
+const searchTerm = document.querySelector("input")
+const songDivs = document.getElementsByClassName('song')
 
-function fetchData(){
-  let searchTerm = document.querySelector("input").value.replace(" ", "+");
-  let url = `https://itunes.apple.com/search?term=${searchTerm}&limit=25`
-  console.log(searchTerm);
 
+const fetchData = () => {
+  let url = `https://itunes.apple.com/search?term=${searchTerm.value}&limit=25`
+  clearResults()
   fetch(url)
   .then((response) => response.json())
   .then(data => {
 
     for (let i = 0; i < data.results.length; i++) {
-      const songLi = document.createElement("li");
+      const songDiv = document.createElement("div");
       const artworkUrl = document.createElement("img");
-      const trackName = document.createElement("p");
-      const artistName = document.createElement("p");
+      const trackName = document.createElement("h3");
+      const artistName = document.createElement("h3");
 
       artworkUrl.src = data.results[i].artworkUrl100;
       trackName.textContent = data.results[i].trackName;
       artistName.textContent = data.results[i].artistName;
-      musicPlayer.setAttribute("src", "data.results[i].previewUrl");
-
-      songLi.appendChild(artworkUrl);
-      songLi.appendChild(trackName);
-      songLi.appendChild(artistName);
-      songsUl.appendChild(songLi);
+      songDiv.classList.add('song')
+      songDiv.addEventListener('click', function(){
+        musicPlayer.setAttribute('src', data.results[i].previewUrl)
+      })
+      songDiv.appendChild(trackName);
+      songDiv.appendChild(artistName);
+      songDiv.appendChild(artworkUrl);
+      results.appendChild(songDiv);
     }
   })
 }
 
-fetchData();
+function clearResults(){
+  results.innerHTML = ""
+}
+searchButton.addEventListener('click', fetchData);
